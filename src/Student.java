907dd7f0ac7cf2b9805a019f8c8861bbeb90b207
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-02-10 23:27:33 -0500

    new instance with validation Lab 2

--- src/Student.java
+++ src/Student.java
@@ -1,33 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class Student {
 //    three private variables.
     private String firstName;
     private String lastName;
     private int studentNumber;
+    private ArrayList<String> interests;
+
+    /*
+    The Constructor takes 4 argument two of String type, one integer type and one is ArrayList<String> type.
+     */
+    public Student(String firstName, String lastName, int studentNumber, ArrayList<String> interest){
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+        setInterest(interest);
+    }
 
 //    setter and getter for each variable.
     public void setFirstName(String firstName){
+        if (firstName.equals(firstName.toUpperCase()))
         this.firstName = firstName;
+        else
+            throw new IllegalArgumentException(firstName+" is invalid please write your First name in CAPITAL LETTERS ");
     }
     public String getFirstName(){
         return firstName;
     }
     public void setLastName(String lastName){
+        if(lastName.equals(lastName.toUpperCase()))
         this.lastName = lastName;
+        else
+            throw new IllegalArgumentException(lastName+" is not valid, Please write your last name in CAPITAL LETTERS");
     }
     public  String getLastName(){
         return lastName;
     }
     public void setStudentNumber(int studentNumber){
+        if(studentNumber<100000&&studentNumber>9999)
         this.studentNumber = studentNumber;
+        else
+            throw new IllegalArgumentException(studentNumber+" is not valid, please type a correct five digit number");
     }
     public int getStudentNumber(){
         return studentNumber;
     }
-//    Constructer with three parameters.
-    public Student(String firstName, String lastName, int studentNumber){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.studentNumber = studentNumber;
+
+    /*
+    Static method which returns an ArrayList of type String and returns the same type of data.
+     */
+    public static ArrayList<String> availableInterest()
+    {
+        ArrayList<String> interests = new ArrayList<>();//new ArrayList or we can use List here
+        interests.add("hiking");//all the valid inputs
+        interests.add("skiing");
+        interests.add("reading");
+        interests.add("coding");
+        interests.add("gaming");
+        interests.add("music");
+        interests.add("dancing");
+        interests.add("sports");
+        return interests;
     }
+
+    public void setInterest(ArrayList<String> interests) {
+        ArrayList<String> validInterest = availableInterest();
+        for (String interest : interests)//for loop is used to iterate the ArrayList.
+        {
+            if (!validInterest.contains(interest))//condition
+                throw new IllegalArgumentException(interest + " is not valid.  please choose from these options: "
+                        +validInterest);
+        }
+        this.interests = interests;
+    }
+    public ArrayList<String> getInterest(){
+        return interests;
+    }
+
 
 }
--- src/StudentTestHarness.java
+++ src/StudentTestHarness.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StudentTestHarness {
     public static void main(String[]args){
-        Student student = new Student("Akshit", "Deswal", 200454399);
-        System.out.printf("Student First name is %s and Last name is %s,%n his student number is %d ",student.getFirstName(), student.getLastName(),student.getStudentNumber());
+        ArrayList<String> unchartedInterests = new ArrayList<>();//the static method we created in Student class.
+        unchartedInterests.addAll(Arrays.asList("music", "sports","hiking","skiing"));
+        Student student = new Student("AKSHIT", "DESWAL", 12345, unchartedInterests );//student object
+//        invalid input
+//        unchartedInterests.addAll(Arrays.asList("sleeping"));
+//        student.setInterest(unchartedInterests);
+        System.out.printf("Student First name is %s and Last name is %s,%n his student number is %d and he loves %s",student.getFirstName(), student.getLastName(),student.getStudentNumber(),student.getInterest());
 
 
     }
